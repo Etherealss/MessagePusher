297bcad85a4a07a118d095478d8eb632224ff280
etherealss <etherealss@163.com>
2023-02-07 09:47:08 +0800

    feature: 重写 ServiceInstanceListSupplier 实现消息路由的自定义负载均衡器

--- mp-connect/src/main/java/cn/wtk/mp/connect/infrastructure/lb/MsgRouteHandler.java
+++ mp-connect/src/main/java/cn/wtk/mp/connect/infrastructure/lb/MsgRouteHandler.java
@@ -1,10 +1,13 @@
 package cn.wtk.mp.connect.infrastructure.lb;
 
-import cn.wtk.mp.common.base.exception.internal.BugException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.client.loadbalancer.Request;
-import org.springframework.cloud.client.loadbalancer.RequestDataContext;
 import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,24 +20,31 @@ import java.util.List;
 @Slf4j
 public class MsgRouteHandler {
 
-    public static final String HEADER_NAME_MSG_ROUTE_IP = "msg-route-ip";
-    public static final String HEADER_NAME_MSG_ROUTE_PORT = "msg-route-port";
+    public static final String HEADER_NAME_MSG_ROUTE_IP = "x-msg-route-ip";
+    public static final String HEADER_NAME_MSG_ROUTE_PORT = "x-msg-route-port";
 
     public void setMsgRouteHeader() {
 
     }
 
-    public boolean needMsgRoute(Request request) {
-        HttpHeaders headers = ((RequestDataContext) request.getContext()).getClientRequest().getHeaders();
+    @Nullable
+    public MsgRouteAddress getMsgRouteAddress(@NonNull HttpHeaders headers) {
         List<String> serverIp = headers.get(HEADER_NAME_MSG_ROUTE_IP);
         List<String> serverPort = headers.get(HEADER_NAME_MSG_ROUTE_PORT);
         if (serverIp == null && serverPort == null) {
-            return false;
+            return null;
         }
         if (serverIp == null || serverPort == null || serverIp.size() != 1 || serverPort.size() != 1) {
             log.warn("用于消息路由的 ip 和 port 出现多个：IPs: {}, ports: {}", serverIp, serverPort);
-            return false;
+            return null;
         }
-        return true;
+        return new MsgRouteAddress(serverIp.get(0), Integer.parseInt(serverPort.get(0)));
     }
+}
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@AllArgsConstructor
+class MsgRouteAddress {
+    String ip;
+    int port;
 }
--- mp-connect/src/main/java/cn/wtk/mp/connect/infrastructure/lb/MsgRouteServiceInstanceLoadBalancer.java
+++ mp-connect/src/main/java/cn/wtk/mp/connect/infrastructure/lb/MsgRouteServiceInstanceLoadBalancer.java
@@ -1,27 +1,78 @@
 package cn.wtk.mp.connect.infrastructure.lb;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.Request;
-import org.springframework.cloud.client.loadbalancer.Response;
-import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
-import reactor.core.publisher.Mono;
+import org.springframework.cloud.client.loadbalancer.RequestData;
+import org.springframework.cloud.client.loadbalancer.RequestDataContext;
+import org.springframework.cloud.loadbalancer.core.DelegatingServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.core.RequestBasedStickySessionServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import reactor.core.publisher.Flux;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author wtk
  * @date 2023/2/6
+ * @see RequestBasedStickySessionServiceInstanceListSupplier 写法参考该类
  */
-@RequiredArgsConstructor
 @Slf4j
-public class MsgRouteServiceInstanceLoadBalancer implements ReactorServiceInstanceLoadBalancer  {
+public class MsgRouteServiceInstanceLoadBalancer extends DelegatingServiceInstanceListSupplier {
 
-    private final ReactorServiceInstanceLoadBalancer delegate;
 
     private final MsgRouteHandler msgRouteHandler;
 
+    public MsgRouteServiceInstanceLoadBalancer(ServiceInstanceListSupplier delegate, MsgRouteHandler msgRouteHandler) {
+        super(delegate);
+        this.msgRouteHandler = msgRouteHandler;
+    }
+
     @Override
-    public Mono<Response<ServiceInstance>> choose(Request request) {
-        return null;
+    public Flux<List<ServiceInstance>> get() {
+        return super.delegate.get();
+    }
+
+    @Override
+    public Flux<List<ServiceInstance>> get(Request request) {
+        Object context = request.getContext();
+        if (context instanceof RequestDataContext) {
+            RequestData clientRequest = ((RequestDataContext) context).getClientRequest();
+            HttpHeaders headers = clientRequest.getHeaders();
+            MsgRouteAddress msgRouteAddress = msgRouteHandler.getMsgRouteAddress(headers);
+            if (msgRouteAddress != null) {
+                return super.delegate.get(request).map(serviceInstances ->
+                        this.selectInstance(serviceInstances, msgRouteAddress.getIp(), msgRouteAddress.getPort())
+                );
+            }
+        }
+        return super.get(request);
+    }
+
+    private List<ServiceInstance> selectInstance(List<ServiceInstance> serviceInstances, @NonNull String ip, int port) {
+        Iterator<ServiceInstance> iterator = serviceInstances.iterator();
+
+        ServiceInstance targetServerInstance = null;
+        for (ServiceInstance serverInstance : serviceInstances) {
+            if (ip.equals(serverInstance.getHost()) && port == serverInstance.getPort()) {
+                targetServerInstance = serverInstance;
+                break;
+            }
+        }
+        if (targetServerInstance == null) {
+            if (log.isDebugEnabled()) {
+                log.warn("消息路由时，获取不到 ip 为 {} 且端口号为 {} 的 serverInstance，通过 delegate 返回所有 serverInstance。", ip, port);
+            }
+            return serviceInstances;
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("发现匹配的 serverInstance: {}，ip: {}，port: {}", targetServerInstance.toString(), ip, port);
+            }
+            return Collections.singletonList(targetServerInstance);
+        }
     }
 }
