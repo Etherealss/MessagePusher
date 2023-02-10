968a78431724c9a72c1b547ba2775e1924d61ade
etherealss <etherealss@163.com>
2023-02-10 19:00:44 +0800

    feature: msg-acceptor 模块创建

---
+++ mp-msg-acceptor/src/main/java/cn/wtk/mp/msg/acceptor/domain/acceptor/Msg.java
@@ -0,0 +1,8 @@
+package cn.wtk.mp.msg.acceptor.domain.acceptor;
+
+/**
+ * @author wtk
+ * @date 2023/2/10
+ */
+public class Msg {
+}
---
+++ mp-msg-acceptor/src/main/java/cn/wtk/mp/msg/acceptor/domain/acceptor/MsgAcceptor.java
@@ -0,0 +1,15 @@
+package cn.wtk.mp.msg.acceptor.domain.acceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wtk
+ * @date 2023/2/10
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class MsgAcceptor {
+}
