2d1fbad64d6f6d2661af3b6028f9bd46f3a304f5
王腾坤 <etherealss@163.com>
2023-03-27 17:23:24 +0800

    bugfix: connect服务成功收发消息
    bug 的原因是在安全认证Handler之后，没有继续将消息传递到协议 Handler

--- mp-connect/src/main/java/cn/wtk/mp/connect/domain/conn/server/NettyServerChannelInitializer.java
+++ mp-connect/src/main/java/cn/wtk/mp/connect/domain/conn/server/NettyServerChannelInitializer.java
@@ -31,13 +31,6 @@ public class NettyServerChannelInitializer extends ChannelInitializer<SocketChan
 
     @Override
     protected void initChannel(SocketChannel socketChannel) {
-        // websocket协议配置
-//        WebSocketServerProtocolConfig webSocketConfig = WebSocketServerProtocolConfig.newBuilder()
-//                .websocketPath(nettyServerConfig.getPath())
-//                .allowExtensions(true)
-//                .maxFramePayloadLength(nettyServerConfig.getMaxFrameSize())
-//                .build();
-//        WebSocketServerProtocolHandler webSocketServerProtocolHandler = new WebSocketServerProtocolHandler(webSocketConfig);
         // 协议选择。用于选择WebSocket还是Socket
         socketChannel.pipeline().addLast(new SocketChooseHandler(
                 nettyServerConfig,
@@ -48,27 +41,5 @@ public class NettyServerChannelInitializer extends ChannelInitializer<SocketChan
                 toByteEncoder,
                 toWebSocketFreamEncoder
         ));
-//        socketChannel.pipeline()
-//                .addLast(new LengthFieldBasedFrameDecoder(1024 * 1024, 0, 4, 0, 4, true))
-//                .addLast(channelLogHandler)
-//                .addLast(connAuthHandler)
-//                .addLast(socketStateHandler)
-//                .addLast(toByteEncoder)
-//        ;
-//        socketChannel.pipeline().addLast(
-//                // HTTP请求的解码和编码，用于ws握手
-//                new HttpServerCodec(),
-//                // 处理大数据流
-//                new ChunkedWriteHandler(),
-//                // HttpServerCodec 会在每个HTTP消息中生成多个消息对象，
-//                // 使用下面的对象把多个消息转换为一个单一的 FullHttpRequest 或是 FullHttpResponse
-//                new HttpObjectAggregator(65536),
-//                // 处理 WebSocket 数据压缩
-//                new WebSocketServerCompressionHandler(),
-//                connAuthHandler,
-//                // WebSocket 协议配置
-//                webSocketServerProtocolHandler,
-//                toWebSocketFreamEncoder
-//        );
     }
 }
